0ec154cc242e652f7616b3887b6bd970e454dfad
HasseG <hassegregers@gmail.com>
2024-02-23 11:48:50 +0100

    Implemented TestDocumentService

--- TeDo/TeDo/TeDo.Libraries/Services/TestDocumentService.cs
+++ TeDo/TeDo/TeDo.Libraries/Services/TestDocumentService.cs
@@ -1,26 +1,29 @@
 ﻿
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
+
 namespace TeDo.Libraries;
 
 public class TestDocumentService : ITestDocumentService
 {
+    private readonly IStorageService _storageService;
 
-    public TestDocumentService()
+    public TestDocumentService(IStorageService storageService)
     {
-        
+        _storageService = storageService;
     }
 
     public List<TestDocument> GetAll()
     {
-        throw new NotImplementedException();
+        return _storageService.TestDocuments.ToList() ?? throw new ArgumentNullException(nameof(_storageService.TestDocuments));
     }
 
     public TestDocument? GetById(int id)
     {
-        throw new NotImplementedException();
+        return _storageService.TestDocuments.FirstOrDefault(t => t.Id == id);
     }
 
     public TestDocument? GetBySlug(string slug)
     {
-        throw new NotImplementedException();
+        return _storageService.TestDocuments.FirstOrDefault(t => t.Slug == slug);
     }
 }
