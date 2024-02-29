53f49e310da7c4bb65d1549433a215b8cfee5c62
Mikk391e <Mikkelkrugelarsen@gmail.com>
2024-02-29 09:16:54 +0100

    Added DefinitonAndAbbreviation list in TestDocument

--- TeDo/TeDo/Client/Model/TestDocument.cs
+++ TeDo/TeDo/Client/Model/TestDocument.cs
@@ -7,10 +7,10 @@
         public Participant? Participant { get; set; }
         public string? Introduction { get; set; }
         public string? DocumentSupplied { get; set; }
-        public Dictionary<string, string>? DefinitionAndAbbreviation { get; set; }
         public List<Punch>? Punchlist { get; set; }
         public List<PlaceOfTesting>? PlacesOfTesting { get; set; }
         public List<Revision>? Revisions { get; set; }
+        public List<DefinitionAndAbbreviation>? DefinitionAndAbbreviation { get; set; }
 
     }
 }
